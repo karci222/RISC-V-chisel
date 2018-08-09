2c21cb88c8196e47d46804243a54a0c4b9593db6
karci222 <karci.marso@gmail.com>
2018-08-09 14:17:00 +0200

    Author: Karol Marso
    Subject: Hazard detection unit connected with pipeline
    
    Next step is performing tests

--- src/main/scala/pipelineRV32I/pipeline.scala
+++ src/main/scala/pipelineRV32I/pipeline.scala
@@ -1,23 +1,22 @@
 package pipelineRV32I
 
 import chisel3._
+import isRV32.Instructions._
 
 class rv32Ipipeline(program: Seq[UInt]) extends Module(){
    val io = IO(new Bundle{
        val res = Output(UInt(32.W))
    })
 
-   val instruction_fetch  = Module(new rv32IF())
-   val instruction_memory = Module(new InstructionRom(program))
-   val instruction_decode = Module(new rv32ID())
-   val registers          = Module(new RegistryFile())
-   val execute            = Module(new rv32EX())
-   val mem                = Module(new rv32MEM()) 
-   val write_back         = Module(new rv32WB()) 
-   val forwarding_unit    = Module(new ForwardingUnit())
-
-    
-
+   val instruction_fetch     = Module(new rv32IF())
+   val instruction_memory    = Module(new InstructionRom(program))
+   val instruction_decode    = Module(new rv32ID())
+   val registers             = Module(new RegistryFile())
+   val execute               = Module(new rv32EX())
+   val mem                   = Module(new rv32MEM()) 
+   val write_back            = Module(new rv32WB()) 
+   val forwarding_unit       = Module(new ForwardingUnit())
+   val hazard_detection_unit = Module(new HazardDetectionUnit())
 
    //IF_ID_registers
    val if_id_NPC_reg = RegInit(0.U(32.W))
@@ -77,6 +76,7 @@ class rv32Ipipeline(program: Seq[UInt]) extends Module(){
    instruction_fetch.io.condIn 	:= ex_mem_condition_reg
    instruction_fetch.io.nextPC  := ex_mem_res_reg
    instruction_fetch.io.instrIn := ex_mem_IR_reg
+   instruction_fetch.io.stall   := hazard_detection_unit.io.stall
  
    instruction_decode.io.instrIn := if_id_IR_reg
 
@@ -100,12 +100,12 @@ class rv32Ipipeline(program: Seq[UInt]) extends Module(){
    write_back.io.dataIn  := mem_wb_lmd_reg
    write_back.io.NPCIn   := mem_wb_NPC_reg
 
+
+   //forwarding part
    forwarding_unit.io.reg1        := id_ex_reg1_reg
    forwarding_unit.io.reg2        := id_ex_reg2_reg
    forwarding_unit.io.ex_mem_inst := ex_mem_IR_reg
    forwarding_unit.io.mem_wb_inst := mem_wb_IR_reg
-
-   
 
    when(forwarding_unit.io.forward_A === "b10".U){
       execute.io.reg1 := ex_mem_res_reg
@@ -121,6 +121,18 @@ class rv32Ipipeline(program: Seq[UInt]) extends Module(){
       execute.io.reg2 := write_back.io.dataToReg
    }.otherwise{
       execute.io.reg2 := id_ex_B_reg
+   }
+
+   //this can be solved outside the forwarding unit  - fast forwarding between load and store instruction
+   when((ex_mem_IR_reg(6,0) === OPCODE_STORE && mem_wb_IR_reg(6,0) === OPCODE_LOAD) && (ex_mem_IR_reg(11,7) === mem_wb_IR_reg(24,20))){
+      mem.io.dataIn := mem_wb_lmd_reg
+   }
+
+   hazard_detection_unit.io.id_ex_instr := id_ex_IR_reg
+   hazard_detection_unit.io.if_id_instr := if_id_IR_reg
+   
+   when(hazard_detection_unit.io.stall){
+      if_id_IR_reg := "b00000000000000000000000000010011".asUInt(32.W)
    }
 
    io.res := execute.io.res
